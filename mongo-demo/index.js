dfb188750958f9dcd1e505b302585a79b446dd9f
Shahar Shokrani <shokrano@gmail.com>
2018-11-28 21:54:13 +0200

    moving into calendar

--- mongo-demo/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.error("Could not connect to MongoDB", err);
-    });
--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=>{    
-    res.render('index', { 
-        message:'Message', 
-        title:'My title' 
-    });
+app.get('/', function(req, res){
+    console.log('Hello world');
+    res.render('index', {name: 'Shahar'});
+});
+
+app.get('/about', function(req, res){
+    console.log('About router');
+    res.render('about', {name: 'Shahar'});
 });
 
 module.exports = router;
