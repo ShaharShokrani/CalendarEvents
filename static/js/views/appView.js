ee7eee5f8d4c48cda28972df11d37e65ca91d160
Shahar Shokrani <shokrano@gmail.com>
2018-12-09 22:06:24 +0200

    Aggregation of events

--- static/js/views/appView.js
+++ static/js/views/appView.js
@@ -2,17 +2,25 @@
 //TODO: use require.
 window.App = window.App || {};
 
+// App.
+
+// App.vent.on("some:event", function(){
+//     alert("some event was fired!");
+// });
+
+// App.vent.trigger("some:event");
+
 App.AppView = Backbone.View.extend({
     el: 'body',
-    model: App.AppModel,
     initialize: function(options) {
         console.log('AppView.initialize');
 
-        // if (!options || !options.model || !options.model.isValid()) {
-        //     throw new Error("options params are missing.");
-        // }
-        // this.model =  options.model;
+        if (!options || !options.vent || !(options.vent instanceof Backbone.Events)) {
+            throw new Error("options params are missing.");
+        }
+        this.vent =  options.vent;
         
+        this.vent.on("filter:genre", this.filterEventsView.bind(this));
         this.template = _.template($('.js-app-template').html());
 
         this.render();
@@ -21,17 +29,34 @@ App.AppView = Backbone.View.extend({
         console.log('AppView.render');
 
         this.$el.append(this.template);
+        this.renderEventsView();
+        this.renderFiltersView();
+    },
+    renderEventsView: function() {
+        console.log('AppView.renderEventsView');
 
         let events = new App.EventsCollection;
         events.fetch();
-
         this.eventsView = new App.EventsView({
             model: events,
+            vent: this.vent.bind(this)
         });
+    },
+    filterEventsView: function(res) {
+        console.log("AppView.filterEventsView" + res);
+        //this.eventsView.filter(res);
+        // this.model.reset(this.model.where({
+        //     cat_name: "interviews"
+        //   }));
+    },
+    renderFiltersView: function() {
+        console.log('AppView.renderFiltersView');
 
         let filters = new App.FiltersCollection();
         filters.fetch();
-
-        let filtersView = new App.FiltersView({ model: filters });
+        let filtersView = new App.FiltersView({ 
+            model: filters,
+            vent: this.vent.bind(this)
+        });        
     }
 });
--- static/js/views/filtersView.js
+++ static/js/views/filtersView.js
@@ -4,10 +4,18 @@ window.App = window.App || {};
 
 App.FiltersView = Backbone.View.extend({
     el: '.js-filters',
-
+    events: {
+        "change .js-genre-id": "onGenreChanged"
+    },
     initialize: function(options) {
         console.log("FiltersView.initialize");
         
+        //if (!options || !options.vent || !(options.vent instanceof Backbone.Events)) { //TODO make a genric common view.
+        if (!options || !options.vent) { //TODO make a genric common view.
+            throw new Error("options params are missing.");
+        }
+        this.vent =  options.vent;
+
         this.listenTo(this.model, 'sync', this.renderOptions);
         this.template = $(".js-filters-template").html();
 
@@ -16,12 +24,12 @@ App.FiltersView = Backbone.View.extend({
     render: function() {
         console.log("FiltersView.render");
 
-        //this.model.fetch();
 		this.$el.html(Mustache.render(this.template));
 		return this;
     },
     renderOptions: function() {
         console.log("FiltersView.renderOptions");
+        let self = this;
 
         this.model.models.forEach(function(model, index, list) {
             if (!model.isValid()) {
@@ -31,8 +39,16 @@ App.FiltersView = Backbone.View.extend({
 
             switch (attributes.TypeId) {
                 case 1:
-                    let genreView = new App.FiltersView.Genre({
-                        model: model
+                    self.$el.find('.js-genre-id').append($('<option>', { 
+                        value: -1,
+                        text : "Please select" 
+                    }))
+            
+                    attributes.Options.forEach(function(item){
+                        self.$el.find('.js-genre-id').append($('<option>', { 
+                            value: item.Value,
+                            text : item.Text 
+                        }));
                     });
                     break;
                 default:
@@ -41,29 +57,34 @@ App.FiltersView = Backbone.View.extend({
 
         });
     },
-});
-
-App.FiltersView.Genre = Backbone.View.extend({    
-    el: '.js-filter-genre-id',
-    initialize: function(options) {
-        console.log("FiltersView.Genre.initialize");
-
-        if (!this.model.isValid()) {
-            throw new Error("FiltersView.Genre.render model not valid, model:'" + model.toJSON() + "'");
-        }
-        this.render();
-    },
-    render: function() {
-        console.log("FiltersView.Genre.render");
+    renderGenre: function() {
+        console.log("FiltersView.renderGenre");
         let self = this;
 
-        let attributes = this.model.toJSON();
-        attributes.Options.forEach(function(item){
-            self.$el.append($('<option>', { 
-                value: item.Value,
-                text : item.Text 
-            }));
-        });
+
 		return this;
+    },
+    onGenreChanged: function(event) {
+        console.log('FiltersView.onGenreChanged');
+        this.vent.trigger("genre", event);
     }
 });
+
+// App.FiltersView.Genre = Backbone.View.extend({    
+//     el: '.js-filter-genre-id',
+//     initialize: function(options) {
+//         console.log("FiltersView.Genre.initialize");
+
+//         if (!this.model.isValid()) {
+//             throw new Error("FiltersView.Genre.render model not valid, model:'" + model.toJSON() + "'");
+//         }
+//         this.render();
+
+//     },
+//     render: function() {
+
+//     },
+//     onChange: function(event) {
+//         this.$el.trigger('FiltersView.Genre.change', event);
+//     }
+// });
