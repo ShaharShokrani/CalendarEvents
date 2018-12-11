63430834c548142db0a67147f1dc0b99813197b1
Fastnet509 <fastn509@gmail.com>
2018-12-11 21:24:16 +0200

    rafael 11/12/2018

--- static/js/views/eventModalView.js
+++ static/js/views/eventModalView.js
@@ -5,10 +5,14 @@ window.App = window.App || {};
 App.EventModalView = Backbone.View.extend({
     el:'body',
     initialize: function() {
-        this.template = $(".js-event-modal-template").html();
+        this.template = $('.js-event-modal-template').html();
     },
     render: function() {
-        this.$el.html(Mustache.render(this.template));
+        //this.$el.html(Mustache.render(this.template, this.model.toJSON()));
+        var template = _.template($(".js-event-modal-template").html());
+        this.$el.html(template(this.model.toJSON()));
+
+        
 //this.$el.html(this.template(this.model.toJSON()));
         return this;
     },
