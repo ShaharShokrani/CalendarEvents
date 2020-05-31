58b4f91a35c9bbb357c6277ef840cb339e5903d9
Shahar Shokrani <shokrano@gmail.com>
2020-05-31 22:29:21 +0300

    Remove the node.js files

--- static/js/collections/appCollection.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-window.App = window.App || {};
-
-App.AppCollection = Backbone.Collection.extend({    
-    model: App.AppModel,
-});
--- static/js/collections/filtersCollection.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-window.App = window.App || {};
-
-App.FiltersCollection = Backbone.Collection.extend({    
-    model: App.FilterModel,
-    url: '/filters'
-});
